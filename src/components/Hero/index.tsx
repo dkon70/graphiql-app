99cbe7439be1ae8dbac478918adfd52a67fafbca
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-07 14:20:51 +0500

    feat: add cards info

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,56 +13,56 @@ export default function Hero() {
   const user = false;
   return (
     <>
-    <Header />
-    <div className="container my-4">
-      <div className="flex flex-col   items-center justify-center  text-center">
-        <div className="flex flex-col   items-center justify-center md:flex-row ">
-          <Image
-            src={image}
-            alt="Rick and Morty"
-            width={280}
-            className="my-auto"
-          />
+      <Header />
+      <div className="container my-4">
+        <div className="flex flex-col   items-center justify-center  text-center">
+          <div className="flex flex-col   items-center justify-center md:flex-row ">
+            <Image
+              src={image}
+              alt="Rick and Morty"
+              width={280}
+              className="my-auto"
+            />
 
-          <div className="lg:w-7/12">
-            <h1 className="mb-6 text-3xl font-medium ">
-              Welcome to <span className="text-green-500">Rick and Morty</span>{' '}
-              GraphQl
-            </h1>
-            <p className="px-2">
-              Welcome to our GraphQL-powered website! This is the main page
-              where you can explore the amazing world of the Rick and Morty API
-              using GraphQL. Discover information about your favorite
-              characters, episodes, and more. Get ready for an exciting journey
-              through the multiverse!
-            </p>
+            <div className="lg:w-7/12">
+              <h1 className="mb-6 text-3xl font-medium ">
+                Welcome to{' '}
+                <span className="text-green-500">Rick and Morty</span> GraphQl
+              </h1>
+              <p className="px-2">
+                Welcome to our GraphQL-powered website! This is the main page
+                where you can explore the amazing world of the Rick and Morty
+                API using GraphQL. Discover information about your favorite
+                characters, episodes, and more. Get ready for an exciting
+                journey through the multiverse!
+              </p>
+            </div>
           </div>
-        </div>
 
-        {user ? (
-          <Link className="my-10" href={'/main'}>
-            Dashboard
-          </Link>
-        ) : (
-          <div className="flex space-x-4 my-6">
-            <Link
-              href={'/login'}
-              className={cn(buttonVariants(), 'py-4 px-6 text-sm')}
-            >
-              Login
+          {user ? (
+            <Link className="my-10" href={'/main'}>
+              Dashboard
             </Link>
-            <Link href={'/register'} className={buttonVariants()}>
-              Register
-            </Link>
+          ) : (
+            <div className="flex space-x-4 my-6">
+              <Link
+                href={'/login'}
+                className={cn(buttonVariants(), 'py-4 px-6 text-sm')}
+              >
+                Login
+              </Link>
+              <Link href={'/register'} className={buttonVariants()}>
+                Register
+              </Link>
+            </div>
+          )}
+          <Separator className="my-4" />
+          <div className="flex flex-col  md:flex-row items-center justify-center  text-center">
+            <AboutSection />
           </div>
-        )}
-        <Separator className="my-4" />
-        <div className="flex flex-col  md:flex-row items-center justify-center  text-center">
-          <AboutSection />
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/ui/AboutSection.tsx
+++ src/components/ui/AboutSection.tsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import Card from './Card';
-import image from 'src/images/rick-and-morty.png';
+import dev1 from 'src/images/dev1.jpg';
+import dev2 from 'src/images/dev2.png';
+import dev3 from 'src/images/dev3.png';
 import { Separator } from '@/components/ui/separator';
 
 const AboutSection = () => {
   const cards = [
     {
       id: 1,
-      photo: image,
-      name: 'Developer 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      photo: dev1,
+      name: 'Dmitry Konev',
+      description:
+        "Aspiring web developer diving into the world of React. Eager to learn and create interactive user interfaces. Let's code! 🚀",
     },
     {
       id: 2,
-      photo: image,
-      name: 'Developer 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      photo: dev2,
+      name: 'Natalia Lebedeva',
+      description:
+        'Natalia has a higher education in construction, but has been interested in programming all her life. She started her path in IT with Python and machine learning, and then got acquainted with web development and realized that she had found her direction. Now she does what she is really interested in, what energizes and inspires her.',
     },
     {
       id: 3,
-      photo: image,
-      name: 'Developer 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      photo: dev3,
+      name: 'Nikita Starmoussov',
+      description:
+        "Nikita, another of our promising junior frontend developers, is the embodiment of ambition and skill. Young yet incredibly adept, his drive to be the best is palpable in every task he undertakes.. Every line of code he writes echoes his dedication and eagerness to excel. With such passion, there's no doubt that the sky's the limit for him in the tech world.",
     },
   ];
 
   return (
     <section className="py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 className="text-2xl font-bold text-green-500 mb-8">About Team</h2>
+        <h2 className="text-2xl font-bold text-green-500 mb-8">About Team</h2>
         <p className="text-gray-600 pb-8">
-        Our frontend development team is a group of talented and dedicated
+          Our frontend development team is a group of talented and dedicated
           professionals who are passionate about creating exceptional user
           experiences. With their expertise in HTML, CSS, and JavaScript, they
           bring websites and web applications to life. The team is committed to
