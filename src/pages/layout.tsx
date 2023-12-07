eab31aceaa46eb5778aecb435b39255004ccf3a6
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-07 14:55:45 +0500

    fix: layout fix

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,11 +2,11 @@ import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { ReactNode } from 'react';
 
-const Layout = ({ children }: { children?: ReactNode }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Header />
-      <div className="min-h-[calc(100vh-160px)]">{children}</div>
+      <main className="min-h-[calc(100vh-160px)]">{children}</main>
       <Footer />
     </>
   );
