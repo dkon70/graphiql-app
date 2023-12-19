8e9179be1a639ed643cae3f49ef5966d067b3fcf
dkon70 <dkon70@inbox.ru>
2023-12-19 22:23:32 +0700

    feat: add graphiql page

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -4,20 +4,12 @@ const Editor = () => {
   const [lines, setLines] = useState([1]);
   const [textareaValue, setTextareaValue] = useState('');
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const lineNumbersRef = useRef<HTMLDivElement | null>(null);
 
   const linesHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextareaValue(event.target.value);
     const totalLines = event.target.value.split('\n').length;
     setLines([...Array(totalLines).keys()].map((index) => index + 1));
-  };
-
-  const handleTextareaScroll = () => {
-    const textarea = textareaRef.current;
-    const lineNumbers = document.getElementById('lineNumbers');
-
-    if (lineNumbers && textarea) {
-      lineNumbers.scrollTop = textarea.scrollTop;
-    }
   };
 
   const handleTabs = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -31,19 +23,29 @@ const Editor = () => {
       setTextareaValue(newValue);
 
       const newCursorPosition = selectionStart + 1;
-      event.currentTarget.setSelectionRange(
-        newCursorPosition,
-        newCursorPosition
-      );
+      requestAnimationFrame(() => {
+        if (textareaRef.current) {
+          textareaRef.current.setSelectionRange(
+            newCursorPosition,
+            newCursorPosition
+          );
+        }
+      });
+    }
+  };
+
+  const handleTextareaScroll = () => {
+    if (textareaRef.current && lineNumbersRef.current) {
+      lineNumbersRef.current.scrollTop = textareaRef.current.scrollTop;
     }
   };
 
   return (
-    <div className="flex overflow-auto h-[100%] bg-slate-700 overflow-y-auto py-2">
+    <div className="flex overflow-auto h-[100%] w-[100%] bg-slate-700 overflow-y-auto py-2">
       <div
-        id="lineNumbers"
+        ref={lineNumbersRef}
         data-testid="lineNumbers"
-        className="w-10 h-[100%] flex flex-col justify-start items-center overflow-hidden top-0"
+        className="w-10 flex flex-col justify-start items-center overflow-hidden top-0"
       >
         {lines.map((line) => (
           <span key={line} className="text-gray-500">
--- src/pages/graphiql.tsx
+++ src/pages/graphiql.tsx
@@ -1,11 +1,116 @@
 import Layout from './layout';
 import Editor from '@/components/Editor/Editor';
+import JSONViewerButtons from '@/components/JSONViewerButtons/JSONViewerButtons';
+import upArrow from '../../public/up-arrow.svg';
+import downArrow from '../../public/down-arrow.svg';
+import { useState } from 'react';
+import Image from 'next/image';
+import docsButton from '../../public/docs.svg';
 
 const Graphiql = () => {
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+  const [isVariablesOpen, setVariablesOpen] = useState(false);
+  const [isHeadersOpen, setHeadersOpen] = useState(false);
+  const [isDocsOpen, setDocsOpen] = useState(false);
+
+  const arrowClickHandler = () => {
+    if (isEditorOpen) {
+      setIsEditorOpen(false);
+    } else {
+      setIsEditorOpen(true);
+    }
+  };
+
+  const variablesButtonHandler = () => {
+    if (!isEditorOpen) {
+      setIsEditorOpen(true);
+    }
+    setHeadersOpen(false);
+    setVariablesOpen(true);
+  };
+
+  const headersButtonHandler = () => {
+    if (!isEditorOpen) {
+      setIsEditorOpen(true);
+    }
+    setHeadersOpen(true);
+    setVariablesOpen(false);
+  };
+
+  const docsOpenHandler = () => {
+    if (isDocsOpen) {
+      setDocsOpen(false);
+    } else {
+      setDocsOpen(true);
+    }
+  };
+
   return (
     <Layout>
-      <div className="w-[100%] h-[500px]">
-        <Editor />
+      <div className="flex">
+        <div
+          className={`bg-slate-700 h-[calc(100vh-160px)] border-r border-solid border-gray-500 py-2 px-2`}
+        >
+          <button
+            className={` ${
+              isDocsOpen && 'bg-slate-600'
+            } bg-slate-700 hover:bg-slate-600 rounded h-10 w-10 p-1 duration-150`}
+            onClick={docsOpenHandler}
+          >
+            <Image src={docsButton} alt="docs" />
+          </button>
+        </div>
+        {isDocsOpen && (
+          <div className="w-[500px] bg-slate-700 border-r border-solid border-gray-500 py-2 px-5">
+            <h3 className="text-white text-3xl">Docs</h3>
+          </div>
+        )}
+        <div className="flex-col w-[50%]">
+          <div className={`flex ${isEditorOpen ? 'h-[500px]' : 'h-[730px]'}`}>
+            <Editor />
+            <JSONViewerButtons />
+          </div>
+          <div
+            className={`bg-slate-700 ${
+              isEditorOpen
+                ? 'h-[calc(100vh-160px-500px)]'
+                : 'h-[calc(100vh-160px-730px)]'
+            } border-t border-solid border-gray-500 px-2 flex-col py-2`}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex justify-around w-[20%]">
+                <button
+                  className={`bg-slate-700 ${
+                    isVariablesOpen ? 'text-white' : 'text-gray-300'
+                  } hover:bg-slate-600 px-2 py-1 rounded duration-150`}
+                  onClick={variablesButtonHandler}
+                >
+                  Variables
+                </button>
+                <button
+                  className={`bg-slate-700 ${
+                    isHeadersOpen ? 'text-white' : 'text-gray-300'
+                  } hover:bg-slate-600 px-2 py-1 rounded duration-150`}
+                  onClick={headersButtonHandler}
+                >
+                  Headers
+                </button>
+              </div>
+              <button
+                onClick={arrowClickHandler}
+                className="bg-slate-700 w-10 h-10 hover:bg-slate-600 duration-150 p-1 rounded"
+              >
+                <Image src={isEditorOpen ? downArrow : upArrow} alt="Arrow" />
+              </button>
+            </div>
+            {isEditorOpen && (
+              <div className="h-[calc(100vh-160px-500px-80px)] w-[100%]">
+                <Editor />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="w-[50%] h-[calc(100vh-160px)] bg-slate-600"> </div>
       </div>
     </Layout>
   );
