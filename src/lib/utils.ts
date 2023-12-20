34e9fa809c07e5b041ad73228efc42115665a4dd
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-20 17:08:53 +0500

    feat: add basic useFetch hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { useEffect, useState } from 'react';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function useFetch(url: string){
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const fetchData = ()=> {
+    setLoading(true)
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }
+  fetchData()
+}, [url]);
+
+console.log(data);
+  return {
+    data,
+    error,
+    loading
+  }
 }
