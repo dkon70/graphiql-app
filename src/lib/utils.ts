90f1f376b7b779e5dec31656124b0b3aea51114c
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-20 19:26:03 +0500

    feat: hardcode request

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,24 +6,55 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function useFetch(url: string){
+const baseQuery = `
+query {
+  characters {
+    results {
+      id
+      name
+      status
+      species
+    }
+  }
+}
+`;
+
+export function useFetch(url = "https://rickandmortyapi.graphcdn.app/" ,query = baseQuery, headers = {}) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
+
+
+
+
+
+
 useEffect(() => {
   const fetchData = ()=> {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
+      },
+      body: JSON.stringify({query})
+    };
+    const request = new Request(url, options);
+    
     setLoading(true)
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
+    fetch(request)
+      .then((response) => (
+        console.log(response),
+        response.json()))
       .then((json) => setData(json))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
   }
   fetchData()
-}, [url]);
+}, []);
 
-console.log(data);
+console.log(error,data,loading);
   return {
     data,
     error,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { useFetch } from '@/lib/utils';
 import Layout from './layout';
 import Hero from '@/components/Main';
 
 export default function Home() {
+  useFetch()
   return(
     <Layout>
       <Hero />
