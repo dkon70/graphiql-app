6d321d5f42049b717710f6db28a660ded764720e
dkon70 <dkon70@inbox.ru>
2023-12-23 01:10:06 +0700

    fix: deploy fix

--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -1,31 +1,48 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseKeys from '../../firebase.config';
 
-const firebaseConfig = {
-  apiKey:
-    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:
-    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId:
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:
-    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId:
-    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ||
-    firebaseKeys.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+let firebaseConfig;
+
+if (
+  process.env.NODE_ENV === 'development' ||
+  process.env.NODE_ENV === 'production'
+) {
+  firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
+} else {
+  const firebaseKeys = require('../../firebase.config').default;
+  firebaseConfig = {
+    apiKey:
+      process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket:
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId:
+      process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId:
+      process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ||
+      firebaseKeys.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
+}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
