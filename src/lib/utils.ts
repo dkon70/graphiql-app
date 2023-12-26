a1d504f17bd5417bfadd7b289c17347388eee8a1
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-26 21:02:17 +0500

    refactor: delete trash

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,27 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getSchema = async () => {
-  // if (query === '') {
-    //   return;
-    // }
-    //   //TODO use redux state
-    
-  const url = '';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    //todo test this request
-    body: JSON.stringify(`{\n  __schema {\n    queryType {\n      name\n      description\n      fields {\n        name\n        description\n        args {\n          name\n          description\n          type {\n            name\n            kind\n          }\n        }\n        type {\n          name\n          kind\n        }\n      }\n    }\n  }\n}`),
-  };
-  const request = new Request(url, options);
-  const req = await fetch(request);
-  const data = await req.json();
 
-  return data.data;
-};
 
 
 
