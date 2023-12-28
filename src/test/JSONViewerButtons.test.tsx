9a84c234f4cdf01bffd0c0a25b51a60e7aaa3337
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-12-28 16:41:10 +0600

    fix: fix test

--- src/test/JSONViewerButtons.test.tsx
+++ src/test/JSONViewerButtons.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import JSONViewerButtons from '@/components/JSONViewerButtons/JSONViewerButtons';
+import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
+import JSONViewerButtons from '@/components/JSONViewerButtons/JSONViewerButtons';
+import store from '@/lib/store/store';
 
 describe('JSONViewerButtons component tests', () => {
   test('renders JSONViewerButtons', () => {
-    render(<JSONViewerButtons />);
+    render(
+      <Provider store={store}>
+        <JSONViewerButtons />
+      </Provider>
+    );
     const buttons = screen.getAllByRole('button');
     expect(buttons).toHaveLength(2);
   });
