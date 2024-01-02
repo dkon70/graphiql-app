00f709a148140bbc38b4f6da000745ee749086c0
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-02 12:03:12 +0600

    refactor: add tests

---
+++ src/test/prettifier.test.tsx
@@ -0,0 +1,30 @@
+import { prettifyText } from '@/lib/prettifier';
+
+const text1 = '{hgfjhg   :   {   fjhgkdh:   ghjhgj   }   ,djfhg   :fhgkdj}';
+const text2 = `{
+  hgfjhg: {
+    fjhgkdh: ghjhgj
+  },
+  djfhg: fhgkdj
+}
+`;
+const text3 = `{
+  field(arg: "value") {
+    subField
+  }
+}`;
+const text4 = `{
+  field(arg: "value")  {
+    subField
+  }
+}
+`;
+const text5 = '(}';
+
+describe('Prettifier', () => {
+  test('Works correctly', () => {
+    expect(prettifyText(text1)).toBe(text2);
+    expect(prettifyText(text3)).toBe(text4);
+    expect(prettifyText(text5)).toBeNull();
+  });
+});
