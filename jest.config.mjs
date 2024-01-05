19f1fc10c20a3f2823ef879dbf773968257e9a6f
dkon70 <dkon70@inbox.ru>
2024-01-06 00:57:03 +0700

    fix: prettifier test fix

--- jest.config.mjs
+++ jest.config.mjs
@@ -11,6 +11,11 @@ const config = {
   coverageReporters: ['text', 'lcov', 'clover'],
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
   coverageDirectory: path.join(process.cwd(), 'coverage'),
+  coveragePathIgnorePatterns: [
+    'src/test/setup.ts',
+    'lib/store',
+    'src/pages/_document.tsx',
+  ],
 };
 
 export default createJestConfig(config);
