0b091a0913464a023de1cbf44cea8f66e6e788f1
dkon70 <dkon70@inbox.ru>
2024-01-07 03:42:12 +0700

    fix: editor line overflow behavior fix

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -173,7 +173,10 @@ const Main = () => {
               </div>
             ) : null}
           </section>
-          <section className="w-full" data-testid="section">
+          <section
+            className="w-[calc(50vw-56px)] max-md:w-[calc(100vw-56px)]"
+            data-testid="section"
+          >
             <QueryEditor />
           </section>
         </div>
@@ -181,7 +184,10 @@ const Main = () => {
           <section data-testid="section">
             <JSONViewerButtons />
           </section>
-          <section className="w-full" data-testid="section">
+          <section
+            className="w-[calc(50vw-57px)] max-md:w-[calc(100vw-56px)]"
+            data-testid="section"
+          >
             <ResponseViewier />
           </section>
         </div>
